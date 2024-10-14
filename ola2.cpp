589cdbec2cd72ead556ef30b4bf792a8d22cc43c
arwgrvs <goal080813hgtrgna2@cbtis217.edu.mx>
2024-10-14 08:24:45 -0600

    ola-Cambio-fecha-autor

--- ola2.cpp
+++ ola2.cpp
@@ -13,7 +13,7 @@ int main()
     d=sqrt(((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1)));
 
     cout<<"La distancia es: "<<d<<"\n";
-    cout <<"Autor: arwgrvs\n";
-    cout<<"14/10/2028\n";
+    cout <<"Autor: arwgrvs2\n";
+    cout<<"14/10/2028 a. C.\n";
 
 }
